ba5e2c1750224862d23f47a133b956d458ca7367
vedantbhatotia <vedantbhatotia@gmail.com>
2024-06-25 00:10:13 +0530

    landing page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
-
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 export default function Home() {
   return (
-    <>
-    de
-    </>
+    <div>
+    <Header></Header>
+    <Hero></Hero>
+    </div>
   );
 }
