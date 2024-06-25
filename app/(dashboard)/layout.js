da9caa41f79cc769ff0deda3ea747e337900717b
vedantbhatotia <vedantbhatotia@gmail.com>
2024-06-26 00:09:50 +0530

    sidenavbar

--- app/(dashboard)/layout.js
+++ app/(dashboard)/layout.js
@@ -1,9 +1,13 @@
+import SideNav from "./_comp/SideNav"
 export default function Layout({children}){
     return(
         <>
-        <h2>  
-        </h2>
+        <div className="h-full w-64 flex-col fixed inset-y-0 z-50">
+            <SideNav></SideNav>
+        </div>
+        <div className="md:ml-64">
         {children}
+        </div>
         </>
     )
 }
